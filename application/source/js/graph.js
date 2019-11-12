ae941e34b0e1f8611af9de190ef55a8f06ea4fe2
j-berg <j-berg@users.noreply.github.com>
2019-11-12 16:16:02 -0700

    Updated app organization

--- application/source/js/graph.js
+++ application/source/js/graph.js
@@ -34,91 +34,272 @@ http://bl.ocks.org/mbostock/1153292
 https://bl.ocks.org/mbostock/1212215
 */
 
-d3.json("data/test_data.json", function(data) {
+// Change user selection based on input
+var selection = null;
+console.log(selection)
+function selectPathway() {
 
-   // Extract links links from data for downstream use
-   var links = data[0].links;
+  var selection = document.getElementById("pathwayMenu").value;
+  console.log(selection)
+  return selection;
+};
 
-   // Set nodes variables up and extract node data
-   var nodex = {}
-   var nodes = data[0].nodes;
+// Populate dictionary to access component reactions for each pathway
+function make_pathway_dictionary(data) {
 
-   // Make dictionary of node color values and types
-   node_dict = {}
-   type_dict = {}
-   expression_dict = {}
-   display_analytes_dict = {}
-   display_reactions_dict = {}
-   nodes.forEach(function(node) {
+  // Get pathway name and ID
+  var master = data[0].master_reference;
+  var pathways = data[0].pathway_dictionary;
+  var pathway_dict = {}
+  for (var key in pathways) {
 
-       node_dict[node['name']] = node['rgba_js']
-       type_dict[node['name']] = node['type']
-       expression_dict[node['name']] = node['expression']
+    if (key in master) {
+      var name = master[key];
+      pathway_dict[name] = {
+        'id': key,
+        'reactions': pathways[key]
+      };
 
-       if (node['type'] === 'reaction') {
-         display_analytes_dict[node['name']] = 'none'
-         display_reactions_dict[node['name']] = 'inline'
-       } else {
-         display_reactions_dict[node['name']] = 'none'
-         display_analytes_dict[node['name']] = 'inline'
-       };
+    } else {
+      //var name = key; //eventually need to figure out mapping for remaining pathways that dont have a name, maybe they should be R-ALL
+    };
+
+  };
+
+  return pathway_dict;
+};
+
+// Make Pathway menu for users to
+function make_menu(pathway_dict) {
+
+  // Get species names (keys) as list
+  pathways_list = Object.getOwnPropertyNames(
+    pathway_dict
+  ).map(function(k) {
+    return k;
+  });
+  pathways_list.sort();
+  pathways_list.unshift("Select a pathway..."); // Add select prompt to menu bar
+
+  // Generate drop-down menu for species select
+  var menu = document.getElementById("pathwayMenu");
+  for (var i = 0; i < pathways_list.length; i++) {
+
+    var option = document.createElement("option");
+    option.innerHTML = pathways_list[i];
+    option.value = pathways_list[i];
+    menu.appendChild(option);
+
+  };
+
+};
+
+function initialize_nodes(nodes, node_dict, type_dict) {
+
+  var expression_dict = {};
+  var display_analytes_dict = {};
+  var display_reactions_dict = {};
+
+  // Make dictionary of node color values and types
+  nodes.forEach(function(node) {
+
+    node_dict[node['name']] = node['rgba_js']
+    type_dict[node['name']] = node['type']
+    expression_dict[node['name']] = node['expression']
+
+    if (node['type'] === 'reaction') {
+      display_analytes_dict[node['name']] = 'none'
+      display_reactions_dict[node['name']] = 'inline'
+    } else {
+      display_reactions_dict[node['name']] = 'none'
+      display_analytes_dict[node['name']] = 'inline'
+    };
+
+  });
+
+  return node_dict, type_dict, expression_dict, display_analytes_dict, display_reactions_dict;
+
+};
+
+function initialize_links(links, nodex, node_dict, type_dict) {
+
+  // Populate node object with relevant information and data
+  links.forEach(function(link) {
+
+    // Add pertinent node information
+    link.source = nodex[link.source] || (nodex[link.source] = {name: link.source});
+    link.target = nodex[link.target] || (nodex[link.target] = {name: link.target});
+    nodex[link.source["name"]]["color"] = node_dict[link.source["name"]];
+    nodex[link.target["name"]]["color"] = node_dict[link.target["name"]];
+
+    // Prioritize reaction nodes in layout
+    if (type_dict[link.source["name"]] == "reaction") {
+        nodex[link.source["name"]]["weight"] = 100;
+    } else {
+        nodex[link.source["name"]]["weight"] = 1;
+    };
+
+    if (type_dict[link.target["name"]] == "reaction") {
+        nodex[link.target["name"]]["weight"] = 100;
+    } else {
+        nodex[link.target["name"]]["weight"] = 1;
+    };
+
+  });
+
+  return nodex, node_dict;
+
+};
+
+// Toggle zoom and pan by pressing the Alt key
+// This section adapted from Pedro Tabacof, https://stackoverflow.com/a/34815469/9571488
+var toggle_zoom = false;
+function activate_zoom() {
+
+    if (toggle_zoom === true) {
+
+        svg.attr(
+            "transform",
+            "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")"
+
+        );
+    }
+};
+
+function initialize_graph() {
+
+  // Allow flexible window dimensions based on initial window size when opened
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  var svg = d3
+      .select("body")
+      .append("svg")
+          .attr("width", width)
+          .attr("height", height)
+      .call(d3.behavior.zoom()
+      .on("zoom", activate_zoom));
+
+  var force = d3.layout.force()
+      .gravity(0.1)
+      .linkDistance(60)
+      .charge(-500)
+      .on("tick", tick)
+      .size([width, height]);
+
+  return svg, force;
+
+};
+
+// Draw curved edges
+function tick() {
+
+  path.attr("d", linkArc);
+  circle.attr("transform", transform);
+  text.attr("transform", transform);
+
+};
+
+function linkArc(d) {
+
+  var dx = d.target.x - d.source.x;
+  var dy = d.target.y - d.source.y;
+  var dr = Math.sqrt(dx * dx + dy * dy);
+
+  return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+
+};
+
+function transform(d) {
+
+  return "translate(" + d.x + "," + d.y + ")";
+
+};
+
+function parse_pathway(data, reactions) {
+
+  // Extract links links from data for downstream use
+  var links = data[0].links;
+
+  // Set nodes variables up and extract node data
+  var nodes = data[0].nodes;
+
+  var master = data[0].master_reference;
+  var pathways = data[0].pathway_dictionary;
+
+  console.log("===")
+  console.log(reactions)
+  console.log(links)
+  console.log(nodes)
+  console.log(pathways['R-HSA-70555'])
+  console.log(master['R-HSA-70555'])
+
+};
+
+d3.json("data/HSA_global_reactions.json", function(data) {
+
+  var pathway_dict = make_pathway_dictionary(data);
+  make_menu(pathway_dict);
+  d3.select("#pathwayMenu").on("change", change);
+
+  // Graphing
+  function change() {
+
+    var selection = document.getElementById("pathwayMenu").value;
+    var reactions = pathway_dict[selection]['reactions'];
+
+    var nodes, links = parse_pathway(data, reactions);
+
+    // Initialize variables
+    var nodex = {};
+    var node_dict = {};
+    var type_dict = {};
+
+    var node_dict, type_dict, expression_dict, display_analytes_dict, display_reactions_dict = initialize_nodes(nodes, node_dict, type_dict);
+    var nodex, node_dict = initialize_links(links, nodex, node_dict, type_dict);
+
+    // Initialize force graph object
+    //var svg, force = initialize_graph();
+    //var g_nodes = d3.values(nodex);
+
+    // Build graph
+    //force
+    //    .nodes(g_nodes)
+    //    .links(links)
+    //    .start();
 
 
 
-   });
-
-   // Populate node object with relevant information and data
-   links.forEach(function(link) {
-
-       // Add pertinent node information
-       link.source = nodex[link.source] || (nodex[link.source] = {name: link.source});
-       link.target = nodex[link.target] || (nodex[link.target] = {name: link.target});
-       nodex[link.source["name"]]["color"] = node_dict[link.source["name"]];
-       nodex[link.target["name"]]["color"] = node_dict[link.target["name"]];
-
-       // Prioritize reaction nodes in layout
-       if (type_dict[link.source["name"]] == "reaction") {
-           nodex[link.source["name"]]["weight"] = 100;
-       } else {
-           nodex[link.source["name"]]["weight"] = 1;
-       };
-
-       if (type_dict[link.target["name"]] == "reaction") {
-           nodex[link.target["name"]]["weight"] = 100;
-       } else {
-           nodex[link.target["name"]]["weight"] = 1;
-       };
-
-   });
-
-   // Allow flexible window dimensions based on initial window size when opened
-   var width = window.innerWidth;
-   var height = window.innerHeight;
-
-   var svg = d3
-       .select("body")
-       .append("svg")
-           .attr("width", width)
-           .attr("height", height)
-       .call(d3.behavior.zoom()
-       .on("zoom", activate_zoom));
-
-   var force = d3.layout.force()
-       .gravity(0.1)
-       .linkDistance(60)
-       .charge(-500)
-       .on("tick", tick)
-       .size([width, height]);
 
 
-   // Initialize force graph object
-   var g_nodes = d3.values(nodex);
 
-   // Build graph
-   force
-       .nodes(g_nodes)
-       .links(links)
-       .start();
+    d3.select("body")
+        .on("keydown", function () {
+
+            toggle_zoom = d3.event.altKey;
+
+        });
+
+    d3.select("body")
+        .on("keyup", function () {
+
+        toggle_zoom = false;
+
+    });
+
+  };
+
+
+}); //end of json import
+
+   /*
+
+
+
+
+
+
+
 
    // Generate edges with style attributes
    svg.append("defs").selectAll("marker")
@@ -301,61 +482,7 @@ d3.json("data/test_data.json", function(data) {
        .append("path")
            .attr("class", "cell");
 
-   // Draw curved edges
-   function tick() {
-       path.attr("d", linkArc);
-       circle.attr("transform", transform);
-       text.attr("transform", transform);
-   }
-
-   function linkArc(d) {
-
-       var dx = d.target.x - d.source.x,
-           dy = d.target.y - d.source.y,
-           dr = Math.sqrt(dx * dx + dy * dy);
-
-       return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
-
-   }
-
-   function transform(d) {
-
-       return "translate(" + d.x + "," + d.y + ")";
-
-   }
-
-   // Toggle zoom and pan by pressing the Alt key
-   // This section adapted from Pedro Tabacof, https://stackoverflow.com/a/34815469/9571488
-
-   var toggle_zoom = false;
-
-   function activate_zoom() {
-
-       if (toggle_zoom === true) {
-
-           svg.attr(
-               "transform",
-               "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")"
-
-           );
-       }
-   };
-
-   d3.select("body")
-       .on("keydown", function () {
-
-           toggle_zoom = d3.event.altKey;
-
-       });
-
-   d3.select("body")
-       .on("keyup", function () {
-
-       toggle_zoom = false;
-
-   });
-
-   // End of adaption of code by Pedro Tabacof
 
 
-});
+
+   */
