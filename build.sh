6263df1b258810cc86f1bdef47b3876bcf2f26a9
j-berg <j-berg@users.noreply.github.com>
2020-07-01 11:42:04 -0600

    Stage version update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.1.2b
+VERSION=0.1.3b
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Jordan A. Berg, et. al.'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.2-beta'
+release = '0.1.3-beta'
 
 
 # -- General configuration ---------------------------------------------------
