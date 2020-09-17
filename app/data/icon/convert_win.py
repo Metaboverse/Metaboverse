bf773bf07edbf3e09d67feac74392a73027ef913
j-berg <j-berg@users.noreply.github.com>
2020-09-17 10:45:20 -0600

    Update icon

--- app/data/icon/convert_win.py
+++ app/data/icon/convert_win.py
@@ -4,6 +4,8 @@ import os
 import sys
 import pathlib
 import subprocess
+import shutil
+from PIL import Image
 
 if len(sys.argv) < 3:
     print("No path to original / hi-res icon provided")
@@ -15,32 +17,24 @@ if len(sys.argv) > 3:
 
 originalPicture = sys.argv[1]
 ouputDir = sys.argv[2]
+
+originalPicture = "/Users/jordan/Desktop/projects/Metaboverse/app/data/icon/png/icon_512x512.png"
+ouputDir = "/Users/jordan/Desktop/projects/Metaboverse/app/data/icon/win"
+
 if not (os.path.isfile(originalPicture)):
     print(f"There is no such file: {sys.argv[1]}")
     raise SystemExit
 
 fname = pathlib.Path(originalPicture).stem
 ext = pathlib.Path(originalPicture).suffix
-destDir = pathlib.Path(originalPicture).parent
+destDir = pathlib.Path(ouputDir)
 
-iconsetDir = os.path.join(destDir, f"{fname}.iconset")
+iconsetDir = os.path.join(destDir, "metaboverse_logo.ico")
+
 if not (os.path.exists(iconsetDir)):
     pathlib.Path(iconsetDir).mkdir(parents=False, exist_ok=True)
 
-class IconParameters():
-    width = 0
-    scale = 1
-    def __init__(self,width,scale):
-        self.width = width
-        self.scale = scale
-    def getIconName(self):
-        if self.scale != 1:
-            return f"icon_{self.width}x{self.width}{ext}"
-        else:
-            return f"icon_{self.width//2}x{self.width//2}@2x{ext}"
-
-from PIL import Image
-filename = r'logo.png'
-img = Image.open(filename)
+img = Image.open(originalPicture)
 icon_sizes = [(16,16), (32, 32), (48, 48), (64,64), (128,128), (255,255)]
-img.save('logo.ico', sizes=icon_sizes)
+img.save("metaboverse_logo.ico", sizes=icon_sizes)
+shutil.move('metaboverse_logo.ico', 'win/metaboverse_logo.ico')
