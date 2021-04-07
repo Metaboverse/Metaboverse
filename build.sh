ba817208af5cb0bbdbcc154465bfbd05bcc62834
j-berg <j-berg@users.noreply.github.com>
2021-04-07 12:39:37 -0600

    update executable names

--- build.sh
+++ build.sh
@@ -19,20 +19,20 @@ npm test
 cd ..
 
 mv app/python/metaboverse-cli-linux .
-mv app/python/metaboverse-cli-mac .
+mv app/python/metaboverse-cli-darwin .
 cd app
 pwd
 electron-packager ./ Metaboverse --platform=win32 --icon=data/icon/win/metaboverse_logo.ico --overwrite
 cd ..
 
-mv app/python/metaboverse-cli-win.exe .
-mv metaboverse-cli-mac app/python/
+mv app/python/metaboverse-cli-windows.exe .
+mv metaboverse-cli-darwin app/python/
 cd app
 pwd
 electron-packager ./ Metaboverse --platform=darwin --icon=data/icon/mac/metaboverse_logo.icns --overwrite
 cd ..
 
-mv app/python/metaboverse-cli-mac .
+mv app/python/metaboverse-cli-darwin .
 mv metaboverse-cli-linux app/python/
 cd app
 pwd
@@ -40,8 +40,8 @@ electron-packager ./ Metaboverse --platform=linux --icon=data/icon/png/icon_1024
 cd ..
 
 pwd
-mv metaboverse-cli-mac app/python/
-mv metaboverse-cli-win.exe app/python/
+mv metaboverse-cli-darwin app/python/
+mv metaboverse-cli-windows.exe app/python/
 
 mv app/Metaboverse-darwin-x64 ./Metaboverse-darwin-x64-${VERSION}
 mv app/Metaboverse-linux-x64 ./Metaboverse-linux-x64-${VERSION}
@@ -54,7 +54,7 @@ cp app/data/test_data.zip ./Metaboverse-win32-x64-${VERSION}
 chmod +x ./Metaboverse-linux-x64-${VERSION}/Metaboverse
 chmod +x ./Metaboverse-linux-x64-${VERSION}/resources/app/python/metaboverse-cli-linux
 
-chmod +x ./Metaboverse-darwin-x64-0.5.0b/Metaboverse.app/Contents/Resources/app/python/metaboverse-cli-mac
+chmod +x ./Metaboverse-darwin-x64-0.5.0b/Metaboverse.app/Contents/Resources/app/python/metaboverse-cli-darwin
 
 zip -r ./Metaboverse-darwin-x64-${VERSION}.zip ./Metaboverse-darwin-x64-${VERSION}
 zip -r ./Metaboverse-linux-x64-${VERSION}.zip ./Metaboverse-linux-x64-${VERSION}
