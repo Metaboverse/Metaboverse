217417b86cc48eaaa8655a8d3910f628b7c81400
Jordan Berg <j-berg@users.noreply.github.com>
2024-11-09 11:36:19 -0800

    Prep for release

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = 'Jordan A. Berg, et. al.'
 author = 'Jordan A. Berg, et. al.'
 
 # The short X.Y version
-version = '0.0'
+version = '0.11'
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.11.5'
 
 
 # -- General configuration ---------------------------------------------------
